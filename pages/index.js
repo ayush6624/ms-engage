ddf33cec58b87d0dee7b58a13f58477878984a12
Ayush Goyal <ayushg1214@gmail.com>
2021-06-18 12:04:46 +0530

    feat: Add basic home page header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,27 @@
+import { Row, Page, Text, Image, Col } from '@geist-ui/react';
+
 export default function Home() {
-	return <div>fkenf</div>;
+	return (
+		<Page>
+			<Page.Content>
+				<Row>
+					<Col span={4}>
+						<Image
+							src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Microsoft_logo.svg/1200px-Microsoft_logo.svg.png"
+							width="80"
+							height="80"
+							alt="logo"
+						/>
+					</Col>
+					<Col>
+						<Text h1>MS Teams</Text>
+					</Col>
+				</Row>
+				<Row>
+					<Text h4>Start or Join a meeting</Text>
+				</Row>
+			</Page.Content>
+		</Page>
+	);
 }
 // page -> row -> row
